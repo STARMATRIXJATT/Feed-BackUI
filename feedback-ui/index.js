1ad4c6962e93ec9baa7100cbcc77eac3e29b0c2a
STARMATRIXJATT <103106307+STARMATRIXJATT@users.noreply.github.com>
2023-02-13 11:06:02 +0530

    Add files via upload

---
+++ feedback-ui/index.js
@@ -0,0 +1,11 @@
+const ratingEls =document.querySelectorAll(".rating");
+
+
+ratingEls.forEach((ratingEl) =>{
+    ratingEl.addEventListener("click", (event) => {
+        console.log(event.target.innerText || event.target.parentNode.innerText);
+        event.target.classList.add("active");
+        
+    });
+});
+
